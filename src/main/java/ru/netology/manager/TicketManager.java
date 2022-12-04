61c182e598a37a098c53838fce4e00c021a396f7
Vasily Rokhlin <rokhlin.vasily@yandex.com>
2022-12-04 22:06:14 +0300

    Added sorting by travel time

--- src/main/java/ru/netology/manager/TicketManager.java
+++ src/main/java/ru/netology/manager/TicketManager.java
@@ -4,6 +4,7 @@ import ru.netology.domain.Ticket;
 import ru.netology.repository.TicketRepository;
 
 import java.util.Arrays;
+import java.util.Comparator;
 
 public class TicketManager {
     private TicketRepository ticketRepository;
@@ -28,6 +29,21 @@ public class TicketManager {
         }
         Arrays.sort(result);
         return result;
+    }
+
+    public Ticket[] findAll(String from, String to, Comparator<Ticket> comparator) {
+        Ticket[] result = new Ticket[0];
+        for (Ticket ticket : ticketRepository.getAll()) {
+            if (matches(ticket, from, to)) {
+                Ticket[] tmp = new Ticket[result.length + 1];
+                System.arraycopy(result, 0, tmp, 0, result.length);
+                tmp[tmp.length - 1] = ticket;
+                result = tmp;
+            }
+        }
+        Arrays.sort(result, comparator);
+        return result;
+
     }
 
     public boolean matches(Ticket ticket, String from, String to) {
